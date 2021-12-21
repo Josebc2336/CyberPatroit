ab287d7eaa7ce0c62b03eaad9e6981e7b068509d
Jose Cabrera <95988658+Josebc2336@users.noreply.github.com>
2021-12-21 15:59:14 -0500

    Create Update.sh

---
+++ Update.sh
@@ -0,0 +1,8 @@
+curl -sL https://github.com/Josebc2336/CyberPatroit/raw/main/10periodic > /etc/apt/apt.conf.d/10periodic
+curl -sL https://github.com/Josebc2336/CyberPatroit/raw/main/10periodic > /etc/apt/apt.conf.d/20auto-updgrades
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
+echo -e "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs) main universe restricted multiverse" > /etc/apt/sources.list
+echo -e "deb-src http://archive.ubuntu.com/ubuntu $(lsb_release -cs) main universe restricted multiverse " >> /etc/apt/sources.list
+echo -e "deb http://security.ubuntu.com/ubuntu/ $(lsb_release -cs)-security restricted main multiverse universe" >> /etc/apt/sources.list
+echo -e "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-updates restricted main multiverse universe" >> /etc/apt/sources.list
+echo -e "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs)-backports restricted main multiverse universe" >> /etc/apt/sources.list
