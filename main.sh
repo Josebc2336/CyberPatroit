1e60f3029bef0037b54798465eb9636fe5ed8177
Jose Cabrera <95988658+Josebc2336@users.noreply.github.com>
2021-12-21 16:56:59 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,2 +1,3 @@
 sudo bash -c "$(curl -sL https://github.com/Josebc2336/CyberPatroit/raw/main/Update.sh)"
 sudo bash -c "$(curl -sL https://github.com/Josebc2336/CyberPatroit/raw/main/ufw.sh)"
+bash -c "$(curl -sL https://github.com/Josebc2336/CyberPatroit/raw/main/user.sh)"
